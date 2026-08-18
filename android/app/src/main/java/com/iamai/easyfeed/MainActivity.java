cec691d47a21bc3682cc17e10895e711d2f0c807
iamai-development <iamai.contato@gmail.com>
2026-08-18 18:24:13 -0300

    Revert "feat: inicializa Capacitor pra gerar o app Android (APK)"
    This reverts commit f5b0d2d5ea574a6a0e5af66551c134e83f4afc73.

--- android/app/src/main/java/com/iamai/easyfeed/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.iamai.easyfeed;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.iamai.easyfeed',
-  appName: 'Easy Feed',
-  webDir: 'dist'
-};
-
-export default config;
