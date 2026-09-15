9743d21d302f5f9d0febd34c8f823931f7469d58
iamai-development <iamai.contato@gmail.com>
2026-09-14 21:08:43 -0300

    Updates

---
+++ assets/chevron-up-DOPMNneq.js
@@ -0,0 +1,1 @@
+import{d as e}from"./client-BB-owPS4.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
---
+++ assets/minus-CvjxS3y_.js
@@ -0,0 +1,1 @@
+import{d as e}from"./client-BB-owPS4.js";var t=e(`minus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}]]);export{t};
